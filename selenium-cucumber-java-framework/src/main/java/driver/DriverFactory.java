884c3e3e112d211fc2699199f5034a4f817effd3
harrasintissar-AD <harras.intissar@gmail.com>
2026-05-27 14:44:07 -0400

    Added GitHub Actions CI pipeline

--- selenium-cucumber-java-framework/src/main/java/driver/DriverFactory.java
+++ selenium-cucumber-java-framework/src/main/java/driver/DriverFactory.java
@@ -3,46 +3,65 @@ package driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.time.Duration;
 
 public class DriverFactory {
 
-    private static DriverFactory instance = null;
-    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+	private static DriverFactory instance = null;
+	private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-    private DriverFactory() {
-        // Private constructor for Singleton
-    }
+	private DriverFactory() {
+		// Private constructor for Singleton
+	}
 
-    public static DriverFactory getInstance() {
-        if (instance == null) {
-            synchronized (DriverFactory.class) {
-                if (instance == null) {
-                    instance = new DriverFactory();
-                }
-            }
-        }
-        return instance;
-    }
+	public static DriverFactory getInstance() {
+		if (instance == null) {
+			synchronized (DriverFactory.class) {
+				if (instance == null) {
+					instance = new DriverFactory();
+				}
+			}
+		}
+		return instance;
+	}
 
-    public static void initializeDriver() {
-        if (driver.get() == null) {
-            WebDriverManager.chromedriver().setup();
-            WebDriver chromeDriver = new ChromeDriver();
-            chromeDriver.manage().window().maximize();
-            chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.set(chromeDriver);
-        }
-    }
+	public static void initializeDriver() {
 
-    public static WebDriver getDriver() {
-        return driver.get();
-    }
+		if (driver.get() == null) {
 
-    public static void quitDriver() {
-        if (driver.get() != null) {
-            driver.get().quit();
-            driver.remove();
-        }
-    }
+			WebDriverManager.chromedriver().setup();
+
+			ChromeOptions options = new ChromeOptions();
+
+			options.addArguments("--headless=new");
+
+			options.addArguments("--disable-gpu");
+
+			options.addArguments("--no-sandbox");
+
+			options.addArguments("--window-size=1920,1080");
+
+			WebDriver chromeDriver = new ChromeDriver(options);
+
+			chromeDriver.manage().window().maximize();
+
+			chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+			driver.set(chromeDriver);
+		}
+	}
+
+	public static WebDriver getDriver() {
+		return driver.get();
+	}
+
+	public static void quitDriver() {
+		if (driver.get() != null) {
+			driver.get().quit();
+			driver.remove();
+		}
+	}
+
 }
