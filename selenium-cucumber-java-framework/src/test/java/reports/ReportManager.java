be021d0aa4cd6f94e75b97cee675f5db8e19af75
harrasintissar-AD <harras.intissar@gmail.com>
2026-05-30 17:39:13 -0400

    Fix screenshot paths in HTML report

--- selenium-cucumber-java-framework/src/test/java/reports/ReportManager.java
+++ selenium-cucumber-java-framework/src/test/java/reports/ReportManager.java
@@ -6,127 +6,97 @@ import java.io.IOException;
 
 public class ReportManager {
 
-    private static final String REPORT_FOLDER =
-            "target/execution-report/";
+	private static final String REPORT_FOLDER = "target/execution-report/";
 
-    private static StringBuilder htmlContent;
+	private static StringBuilder htmlContent;
 
-    public static void cleanOldReports() {
+	public static void cleanOldReports() {
 
-        File folder =
-                new File(REPORT_FOLDER);
+		File folder = new File(REPORT_FOLDER);
 
-        if (folder.exists()) {
+		if (folder.exists()) {
 
-            File[] files =
-                    folder.listFiles();
+			File[] files = folder.listFiles();
 
-            if (files != null) {
+			if (files != null) {
 
-                for (File file : files) {
-                    file.delete();
-                }
-            }
-        }
+				for (File file : files) {
+					file.delete();
+				}
+			}
+		}
 
-        folder.mkdirs();
-    }
+		folder.mkdirs();
+	}
 
-    public static void startScenario(
-            String scenarioName) {
+	public static void startScenario(String scenarioName) {
 
-        htmlContent =
-                new StringBuilder();
+		htmlContent = new StringBuilder();
 
-        htmlContent.append("""
-                <html>
-                <head>
-                    <title>Scenario Report</title>
-                    <style>
-                        body {
-                            font-family: Arial;
-                            margin: 30px;
-                        }
+		htmlContent.append("""
+				<html>
+				<head>
+				    <title>Scenario Report</title>
+				    <style>
+				        body {
+				            font-family: Arial;
+				            margin: 30px;
+				        }
 
-                        img {
-                            width: 100%;
-                            border: 1px solid #ccc;
-                            margin-bottom: 20px;
-                        }
+				        img {
+				            width: 100%;
+				            border: 1px solid #ccc;
+				            margin-bottom: 20px;
+				        }
 
-                        h1 {
-                            color: #333;
-                        }
+				        h1 {
+				            color: #333;
+				        }
 
-                        .step {
-                            margin-top: 25px;
-                        }
-                    </style>
-                </head>
-                <body>
-                """);
+				        .step {
+				            margin-top: 25px;
+				        }
+				    </style>
+				</head>
+				<body>
+				""");
 
-        htmlContent.append(
-                "<h1>Scenario: "
-                        + scenarioName
-                        + "</h1>");
-    }
+		htmlContent.append("<h1>Scenario: " + scenarioName + "</h1>");
+	}
 
-    public static void addStepScreenshot(
-            String stepName,
-            String imagePath) {
+	public static void addStepScreenshot(String stepName, String imagePath) {
 
-        File imageFile =
-                new File(imagePath);
+		File imageFile = new File(imagePath);
 
-        String absolutePath =
-                imageFile.toURI().toString();
+		String imageName = imageFile.getName();
 
-        htmlContent.append(
-                "<div class='step'>");
+		String relativePath = "../screenshots/" + imageName;
 
-        htmlContent.append(
-                "<h3>"
-                        + stepName
-                        + "</h3>");
+		htmlContent.append("<div class='step'>");
 
-        htmlContent.append(
-                "<img src='"
-                        + absolutePath
-                        + "'>");
+		htmlContent.append("<h3>" + stepName + "</h3>");
 
-        htmlContent.append(
-                "</div>");
-    }
+		htmlContent.append("<img src='" + relativePath + "'>");
 
-    public static void finishScenario(
-            String scenarioName,
-            String status) {
+		htmlContent.append("</div>");
+	}
 
-        htmlContent.append(
-                "<h2>Status: "
-                        + status
-                        + "</h2>");
+	public static void finishScenario(String scenarioName, String status) {
 
-        htmlContent.append(
-                "</body></html>");
+		htmlContent.append("<h2>Status: " + status + "</h2>");
 
-        try {
+		htmlContent.append("</body></html>");
 
-            FileWriter writer =
-                    new FileWriter(
-                            REPORT_FOLDER
-                                    + scenarioName
-                                    .replace(" ", "_")
-                                    + ".html");
+		try {
 
-            writer.write(
-                    htmlContent.toString());
+			FileWriter writer = new FileWriter(REPORT_FOLDER + scenarioName.replace(" ", "_") + ".html");
 
-            writer.close();
+			writer.write(htmlContent.toString());
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			writer.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
