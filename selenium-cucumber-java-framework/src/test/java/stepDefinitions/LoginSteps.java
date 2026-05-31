d28e5ac6183ceb7a836598f13a1ae8a52381201a
harrasintissar-AD <harras.intissar@gmail.com>
2026-05-31 10:25:12 -0400

    Add explicit WebDriverWait before checking the new url

--- selenium-cucumber-java-framework/src/test/java/stepDefinitions/LoginSteps.java
+++ selenium-cucumber-java-framework/src/test/java/stepDefinitions/LoginSteps.java
@@ -7,6 +7,9 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.LoginPage;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 public class LoginSteps {
 
@@ -23,13 +26,18 @@ public class LoginSteps {
 	public void user_enters_valid_username_and_password() {
 
 		loginPage.login("standard_user", "secret_sauce");
+		
 	}
 
 	@Then("user should be redirected to inventory page")
 	public void user_should_be_redirected_to_inventory_page() {
+		
+		WebDriverWait wait = new WebDriverWait(DriverFactory.getDriver(), Duration.ofSeconds(10));
+	    wait.until(ExpectedConditions.urlContains("inventory"));
+	    
 
 		String currentUrl = DriverFactory.getDriver().getCurrentUrl();
-		System.out.println("Current URL: " + currentUrl);
+
 		Assert.assertTrue(currentUrl.contains("inventory"), "Login failed!");
 	}
 
