99a8dd11657a42cc4199a49bb05ea6fb23f3bf17
harrasintissar-AD <harras.intissar@gmail.com>
2026-06-13 21:34:50 -0400

    Fix: Import correct TimeoutException from Selenium in LoginPage

--- selenium-cucumber-java-framework/src/main/java/pages/LoginPage.java
+++ selenium-cucumber-java-framework/src/main/java/pages/LoginPage.java
@@ -1,9 +1,9 @@
 package pages;
 
 import java.time.Duration;
-import java.util.concurrent.StructuredTaskScope.TimeoutException;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
